fcc35cb0f47ffc636cd2bf7317e0164da4a61a63
mannybennett <mannybennett99@gmail.com>
2023-08-29 14:01:20 -0400

    Final

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,65 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+
+enum Numbers {
+  One = 1,
+  Two = 2,  
+  Three = 3
+}
+
+enum Mode {
+  Random = "random",
+  Ordered = "ordered"
+}
 
 function App() {
-  const [number, setNumber] = useState<number>(0);
+  const [number, setNumber] = useState<Numbers>(Numbers.One);
+  const [mode, setMode] = useState<Mode>(Mode.Random)
 
-  const handleClick = () => {
-    if (number === 0 || number === 3) {
-      return setNumber(1)
-    } else if (number === 1) {
-      return setNumber(2)
+  const orderedClick = (): void => {
+    if (number === Numbers.Three) {
+      return setNumber(Numbers.One)
+    } else if (number === Numbers.One) {
+      return setNumber(Numbers.Two)
     } else {
-      return setNumber(3)
+      return setNumber(Numbers.Three)
+    };
+  };
+
+  const randomClick = (): void => {
+    let randomNumber: number = Math.random();
+    if (randomNumber <= 0.33 && number !== Numbers.One) {
+      return setNumber(Numbers.One)
+    } else if (randomNumber > 0.33 && randomNumber <= 0.66 && number !== Numbers.Two) {
+      return setNumber(Numbers.Two)
+    } else if (number !== Numbers.Three) {
+      return setNumber(Numbers.Three)
+    } else {
+      return setNumber(Numbers.One)
     }
   };
 
-  useEffect(() => {
-    console.log("Number", number)
-  }, [number]);
+  const modeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMode(e.target.value as Mode)
+  }
 
   return (
     <main className="mainContainer">
       <h1>Stoplight</h1>
       <div className="contentContainer">
         <div className="stoplight">
-          <div className={number === 1 ? "red" : "circle"} />
-          <div className={number === 2 ? "yellow" : "circle"} />
-          <div className={number === 3 ? "green" : "circle"} />
+          <div className={number === Numbers.One ? "red" : "circle"} />
+          <div className={number === Numbers.Two ? "yellow" : "circle"} />
+          <div className={number === Numbers.Three ? "green" : "circle"} />
         </div>
         <div className="interactables">
-          <button onClick={handleClick}>CHANGE</button>
+          <button onClick={mode === Mode.Random ? randomClick : orderedClick}>CHANGE</button>
           <div className="modes">
-            <div>
-              <input type="radio" name="mode" value="random" />
+            <div className="inputGroup random">
+              <input onChange={modeChange} type="radio" name="mode" value={Mode.Random} checked={mode === Mode.Random} />
               <label htmlFor="random">Random</label>
             </div>
-            <div>
-              <input type="radio" name="mode" value="ordered" />
+            <div className="inputGroup ordered">
+              <input onChange={modeChange} type="radio" name="mode" value={Mode.Ordered} checked={mode === Mode.Ordered} />
               <label htmlFor="ordered">Ordered</label>
             </div>            
           </div>
